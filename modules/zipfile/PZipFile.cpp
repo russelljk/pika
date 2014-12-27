942e09f60be3b105102fbcb152eafd0a498d04e7
Russell Kyle <russell.j.kyle@gmail.com>
2014-12-27 17:49:16 -0600

    Added empty zipfile module based on minizip.

---
+++ modules/zipfile/PZipFile.cpp
@@ -0,0 +1,32 @@
+/*
+ *  PZipFile.cpp
+ *  See Copyright Notice in Pika.h
+ */
+#include "PZipFile.h"
+
+#if defined(PIKA_WIN)
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH: break;
+    }
+    return TRUE;
+}
+
+#endif
+
+using namespace pika;
+
+PIKA_MODULE(zipfile, eng, zipfile)
+{
+    GCPAUSE(eng);
+    return zipfile;
+}
---
+++ modules/zipfile/PZipFile.h
@@ -0,0 +1,12 @@
+/*
+ *  PZipFile.h
+ *  See Copyright Notice in Pika.h
+ */
+#ifndef PIKA_ZIPFILE_HEADER
+#define PIKA_ZIPFILE_HEADER
+
+#include "Pika.h"
+#include "unzip.h"
+#include "zip.h"
+
+#endif
